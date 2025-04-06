8d3bfb72e2f424c0241160ba483bcf6bd5cad289
Runyu-Potman <lrysduphy@163.com>
2025-04-06 19:21:56 +0100

    vmin and vmax

--- SAMI_data_cube_quality_cut_functions.py
+++ SAMI_data_cube_quality_cut_functions.py
@@ -224,7 +224,7 @@ def kdc_separation(x_center, y_center, a, b, pa):
 
     kdc = np.ma.masked_array(cleaned_vel_data, mask = ~ellipse_mask)
 
-    plt.imshow(kdc, cmap = 'jet', origin = 'lower')
+    plt.imshow(kdc, cmap = 'jet', origin = 'lower', vmin = vmin, vmax = vmax)
     plt.colorbar()
     plt.xlabel('SPAXEL X')
     plt.ylabel('SPAXEL Y')
