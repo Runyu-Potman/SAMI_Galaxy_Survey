edee0ea839ca1f2dc058fd98297fd47b305a1b7f
Runyu-Potman <lrysduphy@163.com>
2025-04-08 21:08:40 +0100

    description change

--- SAMI_data_cube_age_Z_functions.py
+++ SAMI_data_cube_age_Z_functions.py
@@ -390,8 +390,8 @@ def ppxf_age_z(specNew, goodpixels_nan, ln_lam, noise_value, redshift, filename,
                        goodpixels = goodpixels_nan, component = component, gas_component = gas_component,
                        gas_names = gas_names, reddening = 0, gas_reddening = 0, quiet = True)
 
-    weights = pp_boot.weights[~gas_component]
-    weights = weights.reshape(reg_dim) / weights.sum()
+        weights = pp_boot.weights[~gas_component]
+        weights = weights.reshape(reg_dim) / weights.sum()
 
     mean_lg_age, mean_metal = sps.mean_age_metal(weights)
     ages.append(mean_lg_age)
