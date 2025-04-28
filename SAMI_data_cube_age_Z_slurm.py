08fe42e9de8fe532f462f606f0a730d3cd6fc758
Runyu-Potman <lrysduphy@163.com>
2025-04-28 21:39:56 +0100

    description change

--- SAMI_data_cube_age_Z_slurm.py
+++ SAMI_data_cube_age_Z_slurm.py
@@ -439,9 +439,6 @@ def ppxf_age_z(specNew, goodpixels_nan, ln_lam, noise_value, redshift, filename,
     reduced_chi2_unreg = pp_unreg.chi2
     print(f'Unregularized reduced Chi^2 (initial noise spectrum): {reduced_chi2_unreg:.3f}')
 
-    # degrees of freedom.
-    dof = goodpixels_nan.size
-
     # rescale noise to achieve reduced Chi2 ~ 1.
     noise_rescaled = noise * np.sqrt(reduced_chi2_unreg)
 
