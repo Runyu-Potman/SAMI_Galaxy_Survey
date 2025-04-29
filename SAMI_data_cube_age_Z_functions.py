a4d2d2d6efbd20e3a511a396ace0711f89e6eee4
Runyu-Potman <lrysduphy@163.com>
2025-04-29 18:33:39 +0100

    description change

--- SAMI_data_cube_age_Z_functions.py
+++ SAMI_data_cube_age_Z_functions.py
@@ -447,6 +447,18 @@ def ppxf_age_z(specNew, goodpixels_nan, ln_lam, noise_value, redshift, filename,
     pp_unreg = ppxf(templates = templates, galaxy = galaxy, noise = noise_rescaled, velscale = velscale, start = start,
                     moments = moments, degree = -1, mdegree = 10, lam = lam_gal, lam_temp = sps.lam_temp,
                     goodpixels = goodpixels_nan, component = component, gas_component = gas_component,
+                    gas_names = gas_names, reddening = 0, gas_reddening = 0, clean = True)
+
+    # newly defined goodpixels.
+    goodpixels_nan = pp_unreg.goodpixels.copy()
+
+    # better constrained start value.
+    start = pp_unreg.sol.copy()
+
+    # the forth unreg_pPXF fit with newly defined goodpixels.
+    pp_unreg = ppxf(templates = templates, galaxy = galaxy, noise = noise_rescaled, velscale = velscale, start = start,
+                    moments = moments, degree = -1, mdegree = 10, lam = lam_gal, lam_temp = sps.lam_temp,
+                    goodpixels = goodpixels_nan, component = component, gas_component = gas_component,
                     gas_names = gas_names, reddening = 0, gas_reddening = 0)
 
     start = pp_unreg.sol.copy()
