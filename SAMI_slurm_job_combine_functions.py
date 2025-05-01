6eebf7f3f8230e239b4e7bb5b62c4320caaacfc8
Runyu-Potman <lrysduphy@163.com>
2025-05-01 17:44:27 +0100

    error bar

--- SAMI_slurm_job_combine_functions.py
+++ SAMI_slurm_job_combine_functions.py
@@ -136,6 +136,33 @@ def slurm_job_combine(base_dir, center_x = 24, center_y = 24):
     plt.tight_layout()
     plt.savefig(f'{base_dir}/gradients.png', dpi = 300)
     plt.show()
+
+    # plots showing the error bar.
+    # Plotting
+    plt.figure(figsize = (12, 5))
+
+    # Age Gradient
+    plt.subplot(1, 2, 1)
+    plt.errorbar(r, age_map - 9, yerr = age_std_map, fmt = 'o', markersize = 5, alpha = 0.2, label= 'Pixels',
+                 color = 'blue')
+    plt.plot(r_sorted, fit_age_sorted - 9, 'r-', linewidth = 2, label = f'Fit: slope = {slope_age:.3f}')
+    plt.xlabel('radius (pixels)')
+    plt.ylabel('log(Age(Gyr))')
+    plt.title('age gradient')
+    plt.legend()
+
+    # Metallicity Gradient
+    plt.subplot(1, 2, 2)
+    plt.errorbar(r, metal_map, yerr = metal_std_map, fmt = 'o', markersize = 5, alpha = 0.2, label = 'Pixels',
+                 color = 'green')
+    plt.plot(r_sorted, fit_metal_sorted, 'r-', linewidth = 2, label = f'Fit: slope = {slope_metal:.3f}')
+    plt.xlabel('radius (pixels)')
+    plt.ylabel('[M/H]')
+    plt.title('metallicity gradient')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
 #-----------------------------------------------------------------------------------
 base_dir = 'CATID_adap_binned_age_Z_v02'
 slurm_job_combine(base_dir)
