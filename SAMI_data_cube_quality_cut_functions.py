94b5803a770093e97c1154fee694483c9d6eca4d
Runyu-Potman <lrysduphy@163.com>
2025-05-04 21:22:02 +0100

    description change

--- SAMI_data_cube_quality_cut_functions.py
+++ SAMI_data_cube_quality_cut_functions.py
@@ -167,7 +167,7 @@ def data_cube_clean_snr(fits_path, sn_threshold, emission_free_range, wavelength
 
     # plot the total S/N map before masking.
     plt.imshow(sn, cmap = 'jet', origin = 'lower')
-    plt.colorbar(label = 'S/N within the emission-free wavelength range')
+    plt.colorbar(label = 'S/N within the emission-free wavelength ranges')
     plt.xlabel('SPAXEL X')
     plt.ylabel('SPAXEL Y')
     plt.title('S/N map before any masking')
