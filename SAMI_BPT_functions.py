70473956050a36b8dfd532722e92ba5a220e3fda
Runyu-Potman <lrysduphy@163.com>
2025-05-16 18:35:04 +0100

    description change

--- SAMI_BPT_functions.py
+++ SAMI_BPT_functions.py
@@ -7,6 +7,7 @@ import numpy as np
 import astropy.io.fits as fits
 import matplotlib.pyplot as plt
 from SAMI_kinematcs import plot_vel_or_sig
+from SAMI_stellar_velocity_quality_cut_functions import quality_cut_gaseous_velocity_map_csv
 
 def gas_distribution(gas_fits_path, output_file, threshold = None):
     # load the optical emission line maps (primary map[0] and error map [1]) for each line.
