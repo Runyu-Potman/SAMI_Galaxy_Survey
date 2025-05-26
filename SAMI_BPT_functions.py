344c1378f048e70ffd60ec8680603df122e5669c
Runyu-Potman <lrysduphy@163.com>
2025-05-26 20:57:00 +0100

    description change

--- SAMI_BPT_functions.py
+++ SAMI_BPT_functions.py
@@ -192,8 +192,8 @@ def bpt(
     combined_mask_NII = np.ma.mask_or(combined_mask_NII, np.ma.getmask(NII_err))
 
     # combined_mask_SII is the combined mask for the second BPT plot.
-    combined_mask_SII = np.ma.getmask(Hα_map)
-    combined_mask_SII = np.ma.mask_or(combined_mask_SII, np.ma.getmask(Hα_err))
+    combined_mask_SII = np.ma.getmask(Ha_map)
+    combined_mask_SII = np.ma.mask_or(combined_mask_SII, np.ma.getmask(Ha_err))
 
     combined_mask_SII = np.ma.mask_or(combined_mask_SII, np.ma.getmask(Hβ_map))
     combined_mask_SII = np.ma.mask_or(combined_mask_SII, np.ma.getmask(Hβ_err))
