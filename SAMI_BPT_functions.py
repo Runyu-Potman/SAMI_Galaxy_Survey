8d78fa0a2820a7a484aca7cf3aa413a7be00658f
Runyu-Potman <lrysduphy@163.com>
2025-06-02 18:04:04 +0100

    description change

--- SAMI_BPT_functions.py
+++ SAMI_BPT_functions.py
@@ -88,7 +88,7 @@ def bpt_plot(Ha_map_clean, ax, log_x, log_y, center_x = 25, center_y = 25):
     #normalized_distance = (distance - np.min(distance)) / (np.max(distance) - np.min(distance))
 
     # plot the BPT diagram.
-    ax.scatter(log_x, log_y, c = normalized_distance, cmap = plt.cm.coolwarm,
+    sc = ax.scatter(log_x, log_y, c = distance, cmap = plt.cm.coolwarm,
                     s = 0.5, alpha = 1) # s: marker size
 
 #-------------------------------------------------------------------------------------
