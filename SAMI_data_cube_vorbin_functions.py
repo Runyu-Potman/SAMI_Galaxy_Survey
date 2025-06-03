f6093c3f1b544b8a97984c31c22cf1895ac307b0
Runyu-Potman <lrysduphy@163.com>
2025-06-03 23:08:07 +0100

    description change

--- SAMI_data_cube_vorbin_functions.py
+++ SAMI_data_cube_vorbin_functions.py
@@ -47,9 +47,9 @@ def vorbin_pre_cube_combine(blue_cube_fits, red_cube_fits, output_filename):
         red_header = red_hdul[0].header
 
     # data cube shape for later use.
-    blue_flux_shape_wave = blue_flux.shape[0]
-    blue_flux_shape_y = blue_flux.shape[1]
-    blue_flux_shape_x = blue_flux.shape[2]
+    blue_cube_shape_wave = blue_flux_cube.shape[0]
+    blue_cube_shape_y = blue_flux_cube.shape[1]
+    blue_cube_shape_x = blue_flux_cube.shape[2]
     # debug.
     print('blue data cube shape:', blue_flux_shape_wave, blue_flux_shape_y, blue_flux_shape_x)
 
