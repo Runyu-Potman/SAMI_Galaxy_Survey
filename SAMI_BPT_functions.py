2c8279d03c4e5fe4652edfb713b3c4c125b133c6
Runyu-Potman <lrysduphy@163.com>
2025-06-09 18:42:26 +0100

    description change

--- SAMI_BPT_functions.py
+++ SAMI_BPT_functions.py
@@ -347,9 +347,9 @@ def bpt(
     axs[1, 0].plot(boundary_2_range, boundary_2_values, color = 'black', linestyle = '--', linewidth = 1)
 
     # add labels for each region, ha = 'center' centers the text horizontally at the specified x position.
-    axs[1, 0].text(-0.8, -0.8, 'SF', color = 'grey', fontsize = 10, ha = 'center')
-    axs[1, 0].text(0.0, -1, 'Comp', color = 'salmon', rotation = 90, fontsize = 10, ha = 'center')
-    axs[1, 0].text(0.0, 0.8, 'AGN', color = 'purple', fontsize = 10, ha = 'center')
+    axs[1, 0].text(-0.8, -0.8, 'SF', color = 'grey', fontsize = fontsize, ha = 'center')
+    axs[1, 0].text(0.0, -1, 'Comp', color = 'salmon', rotation = 90, fontsize = fontsize, ha = 'center')
+    axs[1, 0].text(0.0, 0.8, 'AGN', color = 'purple', fontsize = fontsize, ha = 'center')
 
     # set axis labels, title, and limits.
     axs[1, 0].set_xlabel(r'log([NII]/H$\alpha$)', fontsize = 10)
