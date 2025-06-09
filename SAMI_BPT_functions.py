74eaacfdfad48c954cf28889ca5481e4467518bb
Runyu-Potman <lrysduphy@163.com>
2025-06-09 18:43:19 +0100

    description change

--- SAMI_BPT_functions.py
+++ SAMI_BPT_functions.py
@@ -484,8 +484,8 @@ def bpt(
     axs[1, 2].yaxis.set_minor_locator(AutoMinorLocator())
     axs[1, 2].tick_params(axis = 'both', which = 'minor', length = 2, width = 1, direction = 'in')
 
-    cbar = fig.colorbar(sc, ax = axs[1, 2], location = 'right', pad = 0.04)
-    cbar.set_label('Radius (arcsec)', fontsize = 10)
+    cbar = fig.colorbar(sc, ax = axs[1, 2], pad = cbar_pad)
+    cbar.set_label('Radius (arcsec)', fontsize = fontsize, labelpad = labelpad_cbar)
     cbar.ax.yaxis.set_tick_params(direction = 'in')
     '''
 
