14809616a5e396d392e43e4af31ee73a76483c4e
Runyu-Potman <lrysduphy@163.com>
2025-07-13 18:42:21 +0100

    rotate

--- SAMI_MGE.py
+++ SAMI_MGE.py
@@ -76,7 +76,7 @@ def image_cutout(fits_path, ra, dec, scale, cut_size, output_path, vmin = None, 
         rotated_data = cutout.data
 
     # save the output fits file.
-    cutout_hdu = fits.PrimaryHDU(data = cutout.data)
+    cutout_hdu = fits.PrimaryHDU(data = rotated_data)
     hdul = fits.HDUList([cutout_hdu])
     hdul.writeto(output_path, overwrite = True)
 
