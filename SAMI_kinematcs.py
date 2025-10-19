f778aa2070f1a076d6d71c6777d5ce1a4b7f5495
Runyu-Potman <lrysduphy@163.com>
2025-10-19 17:46:34 +0100

    description

--- SAMI_kinematcs.py
+++ SAMI_kinematcs.py
@@ -91,8 +91,8 @@ def plot_vel_or_sig(csv_path, cmap = 'jet', cbar_label = 'km/s', value_type = 'v
     ax.set_xticks(tick_locs)
     ax.set_yticks(tick_locs)
 
-    ax.set_xlabel('Offset [arcsec]', fontsize = fontsize, labelpad = 8)
-    ax.set_ylabel('Offset [arcsec]', fontsize = fontsize, labelpad = label_pad)
+    ax.set_xlabel('Offset (arcsec)', fontsize = fontsize, labelpad = 8)
+    ax.set_ylabel('Offset (arcsec)', fontsize = fontsize, labelpad = label_pad)
 
     # colorbar.
     if show_colorbar:
