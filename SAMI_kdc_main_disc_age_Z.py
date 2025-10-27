ee1fce4ed210e0cb59f4d7887b834bc0ddbb31c3
Runyu-Potman <lrysduphy@163.com>
2025-10-27 14:51:56 +0000

    description

--- SAMI_kdc_main_disc_age_Z.py
+++ SAMI_kdc_main_disc_age_Z.py
@@ -8,6 +8,29 @@ import numpy as np
 #-------------------------------------------------------------------
 def spectrum_age_Z(vel_fits_file, sig_fits_file, combined_data_cube, ellipticity, a_pixel, pa, output_spectrum,
                    sn_threshold = 3, x_center = 25, y_center = 25, vmin = None, vmax = None, kdc = True):
+    '''
+    Extract the co-added spectrum for the kdc region or outside the kdc region. Do the cleaning before the coadding
+    process using the combined (blue and red) data cube.
+
+    Parameters:
+    - vel_fits_file: stellar velocity fits file, just for visualization.
+    - sig_fits_file: stellar velocity dispersion fits file, just for visualization.
+    - combined_data_cube: the combined (blue and red) data cube file.
+    - ellipticity: extracted from the catalog.
+    - a_pixel: kdc radius in pixel, given by kinemetry.
+    - pa: position angle of the kdc or non-kdc region, starting from North, counterclockwise to the receding side.
+    - output_spectrum: path to the output co-added spectrum file.
+    - sn_threshold: s/n threshold cleaning for the combined data cube before co-adding.
+    - x_center: x center of the galaxy.
+    - y_center: y center of the galaxy.
+    - vmin: colorbar.
+    - vmax: colorbar.
+    - kdc: if True, kdc region will be co-added, if False, non-kdc region will be co-added.
+
+    Returns:
+    - None.
+
+    '''
 
     # look into the stellar velocity map for reference when extracting KDC.
     combined_mask, cleaned_vel_data = quality_cut_stellar_velocity_map(
