342f137d9d44d88f28561e8e3d21e803400d4d9b
Runyu-Potman <lrysduphy@163.com>
2025-11-17 17:57:47 +0000

    axs

--- SAMI_MGE.py
+++ SAMI_MGE.py
@@ -168,6 +168,7 @@ def apply_mge(cut_data, level, minlevel, fwhm, Ar, skylev = 0, scale = 0.396, ng
                 ``20**2 -- 40**2`` when ``linear=True`` (default: ``ngauss=30**2=900``).
         Msolar: absolute magnitude of the sun (e.g., 4.68 for SDSS r band).
         mask_map: the mask map given by mask_map function after using sextractor.
+        ax: matplotlib axes object.
 
     Returns:
 
