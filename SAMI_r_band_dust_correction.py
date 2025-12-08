11c3a68d7a8c7377ca3a2155ef716ac6ca9ea38f
Runyu-Potman <lrysduphy@163.com>
2025-12-08 15:27:27 +0800

    description

--- SAMI_r_band_dust_correction.py
+++ SAMI_r_band_dust_correction.py
@@ -207,7 +207,7 @@ def r_band_dust_correction(galaxy_name, target_label, ra, dec, xc, yc, q, kin_pa
     # threshold line.
     ax.plot(xr, yr + E_thresh, color ='blue', lw = 2, linestyle = '--')
     if logm_max is not None:
-        plt.axvline(logm_max, color='grey', linestyle='--', linewidth=2)
+        plt.axvline(logm_max, color='grey', linestyle = '--', linewidth = 2)
 
     # labels & ticks.
     ax.set_xlabel('log semi-major axis distance (arcsec)', fontsize = 10)
