c93a1194d83ab6164dbc34bf6f4adc8acb80a63c
Runyu-Potman <lrysduphy@163.com>
2025-12-09 12:32:22 +0800

    whether to correct for dust or not

--- SAMI_r_band_dust_correction.py
+++ SAMI_r_band_dust_correction.py
@@ -247,7 +247,8 @@ def r_band_dust_correction(galaxy_name, target_label, ra, dec, xc, yc, q, kin_pa
     # correct r flux in the original unit (nanomaggy).
     r_corr = r_data.copy()
     r_mask = (r_data > 0) & np.isfinite(r_data) & dust_mask & np.isfinite(E_gi) & np.isfinite(Ar)
-    r_corr[r_mask] = r_data[r_mask] * (10 ** (0.4 * Ar[r_mask]))
+    if correct_for_dust:
+        r_corr[r_mask] = r_data[r_mask] * (10 ** (0.4 * Ar[r_mask]))
 
     # visualization.
     plt.figure()
