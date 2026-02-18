b8ae64f45b45c96408ac6c7def803807a0364688
Runyu-Potman <lrysduphy@163.com>
2026-02-18 12:36:04 +0000

    colorbar range

--- SAMI_stellar_velocity_quality_cut_functions.py
+++ SAMI_stellar_velocity_quality_cut_functions.py
@@ -301,8 +301,7 @@ def quality_cut_stellar_velocity_map_global(vel_base_dir, sig_base_dir, output_d
 
             plt.figure(figsize = (10, 8))
 
-            vmin = 0.9 * vals.min()
-            vmax = 0.9 * vals.max()
+            vmin, vmax = np.percentile(vals, [5, 95])
 
             plt.imshow(
                 vel_data, origin = 'lower', aspect = 'auto',
