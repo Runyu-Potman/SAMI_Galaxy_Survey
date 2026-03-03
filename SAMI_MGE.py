e215603a849959675a5263c887cec3690790a5eb
Runyu-Potman <lrysduphy@163.com>
2026-03-03 11:14:25 +0000

    new sample

--- SAMI_MGE.py
+++ SAMI_MGE.py
@@ -735,6 +735,8 @@ if __name__ == '__main__':
     axs[0, 0].set_box_aspect(1)
     axs[0, 1].set_box_aspect(1)
     axs[0, 2].set_box_aspect(1)
+    axs[0, 3].set_box_aspect(1)
+    axs[0, 4].set_box_aspect(1)
 
     axs[1, 0].set_box_aspect(1)
     axs[1, 1].set_box_aspect(1)
