11a0450d68c8088366344231f877e80329a4580a
Runyu-Potman <lrysduphy@163.com>
2026-03-09 17:01:17 +0000

    jpg

--- SAMI_kinematcs.py
+++ SAMI_kinematcs.py
@@ -176,7 +176,7 @@ def add_circle(ax, img, linewidth = 1.5):
 
     Parameters:
     - ax: plot position, e.g., axs[0, 0].
-    - img: SDSS optical image path.
+    - img: SDSS optical jpg image path.
     - linewidth: line width of the circle and the scale bar.
     - scale: pixel scale of the sdss optical image (0.4''/pixel by default).
     - label: whether to add the '|-|' label showing the pixel scale or not.
