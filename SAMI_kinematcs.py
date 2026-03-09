27861124b9941f4d0802e4a06426c03e2f49c408
Runyu-Potman <lrysduphy@163.com>
2026-03-09 17:03:00 +0000

    description

--- SAMI_kinematcs.py
+++ SAMI_kinematcs.py
@@ -172,7 +172,9 @@ def plot_vel_or_sig(csv_path, cmap = 'RdBu_r', cbar_label = 'km/s', value_type =
 def add_circle(ax, img, linewidth = 1.5):
     '''
     Add a circle in the e.g. SDSS optical image to show e.g. the field of view of SAMI.
-    Optionally, add direction, scale labels, and ticks to the image.
+    Optionally, add direction, scale labels |-|, and border ticks to the image. Note that the input
+    optical image cut should be a square image and the width (w) and height of the image should
+    satisfy: w = scale_bar (in arcsec) / pixel_scale (0.4''/pixel by default) * (tick_num - 1) for best visualization.
 
     Parameters:
     - ax: plot position, e.g., axs[0, 0].
