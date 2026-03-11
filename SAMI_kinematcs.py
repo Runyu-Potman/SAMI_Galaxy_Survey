2776669bfe592c916c0f70e0c005c82cf69cafa9
Runyu-Potman <lrysduphy@163.com>
2026-03-11 13:58:30 +0000

    bar

--- SAMI_kinematcs.py
+++ SAMI_kinematcs.py
@@ -8,7 +8,7 @@ from PIL import Image
 import cmasher as cmr
 #------------------------------------------------------------------
 def plot_vel_or_sig(csv_path, cmap = 'RdBu_r', cbar_label = 'km/s', value_type = 'vel',
-                    show_colorbar = True, fontsize = 10, bar_fraction = 0.0468, bar_pad = 0.02, label_pad = 0.85,
+                    show_colorbar = True, fontsize = 10, bar_fraction = 0.0467, bar_pad = 0.02, label_pad = 0.85,
                     ax = None, vmin = None, vmax = None, title = None, csv_path_uncut = None,
                     background_alpha = 0.3, PAs = None, line_length = 12.5, line_styles = None,
                     plot_psf = False, psffwhm = 2, text_ul = None):
