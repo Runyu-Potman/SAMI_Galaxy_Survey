0c5725d99e63de2759e7ca89e6ed1ee2ee709cd9
Runyu-Potman <lrysduphy@163.com>
2026-03-11 15:05:00 +0000

    cmap

--- SAMI_kinematcs.py
+++ SAMI_kinematcs.py
@@ -311,7 +311,7 @@ if __name__ == '__main__':
     vel_cmap = cmr.get_sub_cmap('twilight_shifted', 0.05, 0.95)
     vel_cmap_7969 = cmr.get_sub_cmap('twilight_shifted', 0, 0.85)
     vel_cmap_143287 = cmr.get_sub_cmap('twilight_shifted', 0, 0.93)
-    vel_cmap_230776 = cmr.get_sub_cmap('twilight_shifted', 0, 0.70)
+    vel_cmap_230776 = cmr.get_sub_cmap('twilight_shifted', 0, 0.68)
     vel_cmap_9239900248 = cmr.get_sub_cmap('twilight_shifted', 0.15, 0.95)
 
     sig_cmap = cmr.get_sub_cmap('twilight_shifted', 0.05, 0.6)
