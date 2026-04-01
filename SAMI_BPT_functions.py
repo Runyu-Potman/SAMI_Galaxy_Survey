264d0b0f8e03515c5650ffefc2e03c284c359064
Runyu-Potman <lrysduphy@163.com>
2026-04-01 13:35:28 +0100

    axis ratio

--- SAMI_BPT_functions.py
+++ SAMI_BPT_functions.py
@@ -868,9 +868,14 @@ if __name__ == "__main__":
 
     #---------------------------------------------------------------------------
     # same axis ratio.
-    axs[0].set_box_aspect(1)
-    axs[1].set_box_aspect(1)
-    axs[2].set_box_aspect(1)
+    axs[0, 0].set_box_aspect(1)
+    axs[0, 1].set_box_aspect(1)
+    axs[0, 2].set_box_aspect(1)
+    axs[0, 3].set_box_aspect(1)
+    axs[1, 0].set_box_aspect(1)
+    axs[1, 1].set_box_aspect(1)
+    axs[1, 2].set_box_aspect(1)
+    axs[1, 3].set_box_aspect(1)
 
     plt.tight_layout(h_pad = 0.85, w_pad = 0.85)
     plt.savefig('final/gas_kinematics.png', dpi = 1000, bbox_inches = 'tight')
