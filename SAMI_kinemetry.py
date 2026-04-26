634edea876189ab7cb2e99a487f0fa01b53145f7
Runyu-Potman <lrysduphy@163.com>
2026-04-26 18:07:18 +0100

    227266

--- SAMI_kinemetry.py
+++ SAMI_kinemetry.py
@@ -573,8 +573,9 @@ print('227266 PA sigma:', k_227266.er_pa)
 #plot_kinemetry_maps(xbin, ybin, velbin, k_227266)
 #plt.show()
 
-pa_and_k1_plot(k_227266, axs = axs[:, 2], ypa_lim = [0, 200], ypa_tick = (0, 205, 50), yk1_lim = [0, 30], yk1_tick = (0, 30, 5),
-               x_lim = [0, 7.5], x_tick = (0, 7.5, 1), pa1 = 170.54, pa2 = 32.43, pa1_err = 11.58, pa2_err = 5.28)
+pa_and_k1_plot(k_227266, axs = axs[:, 2], ypa_lim = [0, 200], ypa_tick = (0, 205, 50), yk1_lim = [0, 25], yk1_tick = (0, 25, 5),
+               x_lim = [0, 7.5], x_tick = (0, 7.5, 1), pa1 = np.mean(k_227266.pa[:5]), pa2 = np.mean(k_227266.pa[-6:]),
+               pa1_err = np.std(k_227266.pa[:5]), pa2_err = np.std(k_227266.pa[-6:]), label_pad = 0)
 axs[0, 2].set_title('Galaxy 227266', fontsize = 10)
 
 #----------------------------------------------------------------------------------------------------------------------
