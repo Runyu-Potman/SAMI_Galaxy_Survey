6a7acbc62b5fe7062ce278e4aa336eba6fd166e2
Runyu-Potman <lrysduphy@163.com>
2026-04-28 13:00:45 +0100

    print

--- SAMI_kinemetry.py
+++ SAMI_kinemetry.py
@@ -704,9 +704,6 @@ print('9239900248 PA sigma:', k_9239900248.er_pa)
 k_9239900248_extra = kinemetry(xbin = xbin, ybin = ybin, moment = velbin, error = er_velbin, x0 = np.median(xbin), y0 = np.median(ybin), rangeQ = [0.47, 0.67],
                          rangePA = [20, 50], npa = 41, nq = 41, plot = False, scale = 1, ring = 0.2, cover = 0.20)
 
-print('9239900248 PA:', k_9239900248_extra.pa)
-print('9239900248 PA sigma:', k_9239900248_extra.er_pa)
-
 #plot_kinemetry_profiles_velocity(k_9239900248_extra)
 #plot_kinemetry_maps(xbin, ybin, velbin, k_9239900248_extra)
 #plt.show()
