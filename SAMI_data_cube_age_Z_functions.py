81f11d235251f0c72cd5af130153810b0a147c87
Runyu-Potman <lrysduphy@163.com>
2026-04-29 16:10:00 +0100

    no red

--- SAMI_data_cube_age_Z_functions.py
+++ SAMI_data_cube_age_Z_functions.py
@@ -775,7 +775,6 @@ if __name__ == '__main__':
 
             # set masked regions to be NaN.
             blue_spectrum = blue_spectrum.filled(np.nan)
-            red_spectrum = red_spectrum.filled(np.nan)
 
             # around NaN values, sometimes there will be some very small values.
             # for each NaN value (e.g., at index a), set the adjacent values (a-1 and a+1) to NaN as well.
