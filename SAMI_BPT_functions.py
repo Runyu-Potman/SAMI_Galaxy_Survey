2518a9cc3cd93eb06f8c896fcade1feea2536739
Runyu-Potman <lrysduphy@163.com>
2026-05-05 15:19:59 +0100

    17

--- SAMI_BPT_functions.py
+++ SAMI_BPT_functions.py
@@ -868,8 +868,8 @@ if __name__ == "__main__":
                     value_type = 'gas', ax = axs[1, 0], plot_psf = True, psffwhm = 2.108, galaxy_name = '227266')
 
     # we should mask the particular spaxel!
-    plot_vel_or_sig(csv_path = gas_output_file_227266_kinematics, cmap = vel_cmap, cbar_label = 'Velocity (km/s)',
-                    value_type = 'vel', ax = axs[1, 1], PAs = [7], line_length = 9, plot_psf = True, psffwhm = 2.108)
+    plot_vel_or_sig(csv_path = gas_output_file_227266_kinematics, cmap = vel_cmap, cbar_label = r'$V_\mathrm{gas}$ (km/s)',
+                    value_type = 'vel', ax = axs[1, 1], PAs = [17], line_length = 9, plot_psf = True, psffwhm = 2.108)
 
     plot_vel_or_sig(csv_path = gas_output_file_227266_kinematics, cmap = sig_cmap, cbar_label = 'Velocity Dispersion (km/s)',
                     value_type = 'sig', ax = axs[1, 2], plot_psf = True, psffwhm = 2.108)
