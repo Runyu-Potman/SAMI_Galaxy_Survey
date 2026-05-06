213ef56c3c593582ae6d70554e565a44c2381a28
Runyu-Potman <lrysduphy@163.com>
2026-05-06 14:54:14 +0100

    text

--- SAMI_BPT_functions.py
+++ SAMI_BPT_functions.py
@@ -915,12 +915,19 @@ if __name__ == "__main__":
     axs[0, 1].set_title('Gas Velocity', fontsize = 10)
     axs[0, 2].set_title('Gas Velocity Dispersion', fontsize = 10)
     axs[0, 3].set_title('Stellar Velocity', fontsize = 10)
-    axs[0, 1].text(-7, 9, r'$\mathrm{PA}_\mathrm{\,gas}$=$-146.5^\circ$', color = 'black', fontsize = 10, ha = 'left')
-    axs[0, 3].text(1, -0.9, r'$\mathrm{PA}_\bigstar$=$43.13^\circ$', color = 'black', fontsize = 10, ha = 'left')
-    axs[0, 3].text(-4, 8.2, r'$\mathrm{PA}_\bigstar$=$-161.73^\circ$', color = 'black', fontsize = 10, ha = 'left')
-    axs[1, 1].text(-2.5, 9.6, r'$\mathrm{PA}_\mathrm{\,gas}$=$187^\circ$', color = 'black', fontsize = 10, ha = 'left')
-    axs[1, 3].text(-11, -4.4, r'$\mathrm{PA}_\bigstar$=$170.54^\circ$', color = 'black', fontsize = 10, ha = 'left')
-    axs[1, 3].text(-6, 9.1, r'$\mathrm{PA}_\bigstar$=$32.43^\circ$', color = 'black', fontsize = 10, ha = 'left')
+    axs[0, 4].set_title('SFR Surface Density', fontsize = 10)
+
+    axs[0, 1].text(-7.2, 9, r'$\mathrm{PA}_\mathrm{\,gas}$=$-145^\circ$', color = 'black', fontsize = 10, ha = 'left')
+    axs[0, 3].text(0.7, -0.5, r'$\mathrm{PA}_\bigstar$=$44.72^\circ$', color = 'black', fontsize = 10, ha = 'left')
+    axs[0, 3].text(-4.2, 8.8, r'$\mathrm{PA}_\bigstar$=$-160.84^\circ$', color = 'black', fontsize = 10, ha = 'left')
+
+    axs[1, 1].text(-4, 9.5, r'$\mathrm{PA}_\mathrm{\,gas}$=$-163^\circ$', color = 'black', fontsize = 10, ha = 'left')
+    axs[1, 3].text(-11, -4.4, r'$\mathrm{PA}_\bigstar$=$171.75^\circ$', color = 'black', fontsize = 10, ha = 'left')
+    axs[1, 3].text(-6.4, 9.8, r'$\mathrm{PA}_\bigstar$=$34.53^\circ$', color = 'black', fontsize = 10, ha = 'left')
+
+    axs[2, 1].text(-1, 8, r'$\mathrm{PA}_\mathrm{\,gas}$=$127.5^\circ$', color = 'black', fontsize = 10, ha = 'left')
+    axs[2, 3].text(-1.5, -3.8, r'$\mathrm{PA}_\bigstar$=$149.12^\circ$', color = 'black', fontsize = 10, ha = 'left')
+    axs[2, 3].text(-2, 5.5, r'$\mathrm{PA}_\bigstar$=$-53.95^\circ$', color = 'black', fontsize = 10, ha = 'left')
 
     #---------------------------------------------------------------------------
     # same axis ratio.
