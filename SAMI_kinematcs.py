cea795625035831787aa2c19de1edd522a490469
Runyu-Potman <lrysduphy@163.com>
2026-05-06 15:36:54 +0100

    143287

--- SAMI_kinematcs.py
+++ SAMI_kinematcs.py
@@ -356,8 +356,8 @@ if __name__ == '__main__':
     add_circle(axs[1, 0], optical_143287, linewidth = 1, label = True, E_bar = 1.5)
 
     # 143287 kinematics.
-    plot_vel_or_sig(csv_path = star_output_file_143287, value_type = 'vel', ax = axs[1, 1], cmap = vel_cmap_143287, cbar_label = 'Velocity (km/s)', plot_psf = True, fontsize = 10, psffwhm = 2.250, vmin = -85, vmax = 85)
-    plot_vel_or_sig(csv_path = star_output_file_143287, value_type = 'sig', ax = axs[1, 2], cmap = sig_cmap_143287, cbar_label = 'Velocity Dispersion (km/s)', plot_psf = True, fontsize = 10, psffwhm = 2.250, vmin = 55, vmax = 185)
+    plot_vel_or_sig(csv_path = star_output_file_143287, value_type = 'vel', ax = axs[1, 1], cmap = vel_cmap_143287, cbar_label = r'$V_{\bigstar}$ (km/s)', plot_psf = True, fontsize = 10, psffwhm = 2.250, vmin = -85, vmax = 85)
+    plot_vel_or_sig(csv_path = star_output_file_143287, value_type = 'sig', ax = axs[1, 2], cmap = sig_cmap_143287, cbar_label = r'$\sigma_{\bigstar}$ (km/s)', plot_psf = True, fontsize = 10, psffwhm = 2.250, vmin = 55, vmax = 185)
     #--------------------------------------------------------------------------------------------
     # 227266 optical.
     optical_227266 = Image.open('227266/optical/227266_optical_image.jpg')
