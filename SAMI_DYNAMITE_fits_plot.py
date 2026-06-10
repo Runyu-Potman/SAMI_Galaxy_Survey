ca9e979cb161f975dcc31ea47c5cf88508d79d76
Runyu-Potman <lrysduphy@163.com>
2026-06-10 12:22:28 +0100

    15

--- SAMI_DYNAMITE_fits_plot.py
+++ SAMI_DYNAMITE_fits_plot.py
@@ -180,8 +180,8 @@ def reproduce_orbit_plot(fits_file, ax = None, cbar = True, name = None, r_kdc =
     im = ax.imshow(data, origin = 'lower', extent = extent, cmap = 'terrain_r',
                    interpolation = interp, vmin = vmin, vmax = vmax, aspect = 'auto')
 
-    ax.tick_params(direction = 'in')
-    ax.set_xlabel('Radius (arcsec)', fontsize = 10)
+    ax.tick_params(direction = 'in', labelsize = 15)
+    ax.set_xlabel('Radius (arcsec)', fontsize = 15)
     if name is not None:
         ax.set_ylabel(f'Galaxy {name}\nCircularity $\lambda_{{z}}$', fontsize = 10)
     else:
