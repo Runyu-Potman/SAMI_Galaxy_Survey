b8732948ac0096238f7d27750196335bb59a9c0b
Runyu-Potman <lrysduphy@163.com>
2026-06-15 11:28:12 +0100

    .

--- SAMI_DYNAMITE_fits_plot.py
+++ SAMI_DYNAMITE_fits_plot.py
@@ -351,7 +351,7 @@ def plot_kinematic_maps_from_fits(fits_path, number_gh=4):
         plt.subplot(3, n_col, 2*n_col + 2)
         c = get_flat('residual_vel')
         display_pixels.display_pixels(x_flat, y_flat, c,
-                                      vmin=-10, vmax=10,
+                                      vmin = -10, vmax = 10,
                                       **kw_display_pixels)
 
         plt.subplot(3, n_col, 2*n_col + 3)
