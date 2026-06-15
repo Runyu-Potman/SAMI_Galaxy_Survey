943b53245bf3c68fe6019072a92527027d6f59bc
Runyu-Potman <lrysduphy@163.com>
2026-06-15 11:27:46 +0100

    .

--- SAMI_DYNAMITE_fits_plot.py
+++ SAMI_DYNAMITE_fits_plot.py
@@ -319,7 +319,7 @@ def plot_kinematic_maps_from_fits(fits_path, number_gh=4):
         plt.subplot(3, n_col, n_col + 1)
         c = get_flat('model_sb')
         display_pixels.display_pixels(x_flat, y_flat, c,
-                                      vmin=np.nanmin(c), vmax=np.nanmax(c),
+                                      vmin = np.nanmin(c), vmax = np.nanmax(c),
                                       **kw_display_pixels1)
 
         plt.subplot(3, n_col, n_col + 2)
