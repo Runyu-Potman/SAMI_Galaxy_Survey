cea012e2697c08b25c28126178b3bfbcc7e70716
Runyu-Potman <lrysduphy@163.com>
2026-06-16 18:37:35 +0100

    .

--- poster.py
+++ poster.py
@@ -404,6 +404,138 @@ def add_psf(ax, psffwhm):
 
     ax.add_patch(circle)
 #####################################################################################
+def reproduce_mass_plot(fits_filename, ax = None, name = None, r_kdc = None, extrap_start = None, output_plot = None, label = False):
+    '''
+    Reproduce the cumulative mass plot from the FITS file saved by mass_plot().
+
+    Parameters:
+    - fits_filename: str. Path to the FITS file (e.g., 'enclosed_mass_profiles.fits').
+    - ax: matplotlib.axes.Axes.
+    - name: str. Name of the galaxy.
+    - r_kdc: a vertical dotted line to represent the radius of the kinematically distinct component.
+    - extrap_start: start radius for the shaded region representing the extrapolated zone.
+    - output_plot: str, optional. Output filename for the plot.
+    - label: bool, optional. Label the plot.
+
+    Returns:
+    - fig: matplotlib.figure.Figure.
+    '''
+
+    # Read the FITS file (table extension is index 1).
+    with fits.open(fits_filename) as hdul:
+        hdu = hdul[1]  # BinTableHDU
+        data = hdu.data
+        header = hdu.header
+
+    # Extract data columns
+    R_arcsec = data['R_arcsec']
+    #R_pc = data['R_pc']
+    total_best = data['total_best']
+    total_min = data['total_min']
+    total_max = data['total_max']
+    stellar_best = data['stellar_best']
+    stellar_min = data['stellar_min']
+    stellar_max = data['stellar_max']
+    dm_best = data['dm_best']
+    dm_min = data['dm_min']
+    dm_max = data['dm_max']
+
+    # Get header values for plot limits
+    arctpc = header['ARCTPC']
+    Rmax_arcs = header['RMAXARC']
+
+    # Determine if dark matter exists (all dm_best zero -> no DM)
+    has_dm = np.any(dm_best != 0)
+
+    # Compute y-axis upper limit (same as original: round to next 10^10)
+    max_mass = np.max(total_max)
+    ymax = (int(max_mass / 1e10) + 1) * 1e10
+
+    # Define x range for axis limits (same as original)
+    xrange = np.array([0.1, Rmax_arcs])
+    yrange = np.array([1e6, ymax])
+
+    # Create the plot
+    if ax is None:
+       fig, ax = plt.subplots(figsize = (5, 5))
+       created_fig = True
+    else:
+       fig = ax.figure
+       created_fig = False
+
+    ax.set_xlim(xrange)
+    ax.set_ylim(yrange)
+    ax.xaxis.set_major_locator(plt.MaxNLocator(nbins = 4))
+    ax.set_xlabel(r'Radius (arcsec)', fontsize = 10)
+    if name is not None:
+       ax.set_ylabel(f'Enclosed Mass ($M_{{\odot}}$)', fontsize = 10)
+    else:
+       ax.set_ylabel(r'Enclosed Mass ($M_{\odot}$)', fontsize = 10)
+
+    # position of the y axis offset text.
+    offset_text = ax.yaxis.get_offset_text()
+    offset_text.set_position((-0.012, 0.95))
+    offset_text.set_fontsize(10)
+
+    # x and y axis tick settings.
+    ax.tick_params(labelsize = 10, direction = 'in', top = False)
+    # make the tick to have the highest zorder.
+    ax.set_axisbelow(False)
+
+    '''
+    # Twin axis for kpc.
+    ax2 = ax.twiny()
+    ax2.set_xlim(xrange * arctpc / 1000.0)
+    ax2.xaxis.set_major_locator(plt.MaxNLocator(nbins = 4))
+    ax2.set_xlabel(r'$r$ (kpc)', fontsize = 10)
+    ax2.tick_params(labelsize = 10, direction = 'in')
+    ax2.set_axisbelow(False)
+    '''
+
+    # Plot total mass (black).
+    if label is True:
+       ax.plot(R_arcsec, total_best, '-', color = 'k', linewidth = 2.0, label = 'Total')
+    else:
+       ax.plot(R_arcsec, total_best, '-', color = 'k', linewidth = 2.0)
+
+    ax.fill_between(R_arcsec, total_min, total_max, facecolor = 'k', alpha = 0.1)
+
+    # Plot stellar (red) and dark matter (blue) if DM exists
+    if has_dm:
+        if label is True:
+           ax.plot(R_arcsec, stellar_best, '-', color = 'r', linewidth = 2.0, label = 'Mass-follows-Light')
+        else:
+           ax.plot(R_arcsec, stellar_best, '-', color = 'r', linewidth = 2.0)
+
+        ax.fill_between(R_arcsec, stellar_min, stellar_max, facecolor = 'r', alpha = 0.1)
+
+        if label is True:
+           ax.plot(R_arcsec, dm_best, '-', color = 'b', linewidth = 2.0, label = 'Dark Matter')
+        else:
+           ax.plot(R_arcsec, dm_best, '-', color = 'b', linewidth = 2.0)
+
+        ax.fill_between(R_arcsec, dm_min, dm_max, facecolor = 'b', alpha = 0.1)
+
+    if label is True:
+       ax.legend(loc = 'upper left', bbox_to_anchor = (0.01, 0.99), fontsize = 8)
+
+    # a dashed line to represent the radius of the kinematically distinct component.
+    if r_kdc is not None:
+        ax.axvline(r_kdc, color = 'orange', linestyle = 'dotted', linewidth = 2.0)
+
+    # the shaded region to represent the extrapolated region.
+    if extrap_start is not None:
+        # get current x_limits.
+        xlim = ax.get_xlim()
+        # shade from extrap_start to the right limit.
+        ax.axvspan(extrap_start, xlim[1], facecolor = 'none', hatch = '\\\\',
+                   edgecolor = 'gray', alpha = 0.3)
+
+    if output_plot and created_fig:
+        plt.savefig(output_plot)
+
+    idx_kdc = np.argmin(np.abs(R_arcsec - r_kdc))
+    print(f'Galaxy {name}: Enclosed stellar mass at r = {r_kdc} arcsec: {stellar_best[idx_kdc]:.3e} M_sun')
 
 
 galaxy = '300787'
