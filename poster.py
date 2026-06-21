510b1be32c8a81d2cb01658f573b0e75d9a7eae9
Runyu-Potman <lrysduphy@163.com>
2026-06-21 15:21:19 +0100

    z

--- poster.py
+++ poster.py
@@ -720,7 +720,6 @@ add_psf(ax = axs[1, 1], psffwhm = 1.941)
 axs[0, 0].set_box_aspect(1)
 axs[0, 1].set_box_aspect(1)
 axs[0, 2].set_box_aspect(1)
-axs[0, 3].set_box_aspect(1)
 
 axs[1, 0].set_box_aspect(1)
 axs[1, 1].set_box_aspect(1)
