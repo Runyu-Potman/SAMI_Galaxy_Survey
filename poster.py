545f1eaee24bf45007c639a17238787acc34c665
Runyu-Potman <lrysduphy@163.com>
2026-06-25 12:49:41 +0100

    z

--- poster.py
+++ poster.py
@@ -329,7 +329,7 @@ def plot_age_and_Z(axs_x, age_full, metal_full, r_all, age_array, metal_array, a
     axs[axs_x].set_ylabel('Offset (arcsec)', fontsize = fontsize, labelpad = label_pad)
 
     # set color bar (first column).
-    cbar = plt.colorbar(im, ax = axs[axs_x, 2], fraction = bar_fraction, pad = bar_pad)
+    cbar = plt.colorbar(im, ax = axs[axs_x], fraction = bar_fraction, pad = bar_pad)
     cbar.ax.yaxis.set_tick_params(direction = 'in')
     cbar.set_label('Age (Gyr)', fontsize = fontsize, labelpad = 4)
 
