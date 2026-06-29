7e6a624fdf9afe4f243dcbb20813d4a9fd764eda
Runyu-Potman <lrysduphy@163.com>
2026-06-29 16:28:49 +0100

    z

--- SAMI_DYNAMITE_fits_plot.py
+++ SAMI_DYNAMITE_fits_plot.py
@@ -224,7 +224,7 @@ def reproduce_orbit_plot(fits_file, ax = None, cbar = True, name = None, r_kdc =
     return fig
 
 #--------------------------------------------------------------------------------
-def plot_kinematic_maps_from_fits_grid(fits_paths, number_gh = 4, labels = None):
+def plot_kinematic_maps_from_fits_grid(fits_paths, number_gh = 4, labels = None, figsize = (25, 20)):
     '''
     Plot 6 galaxies in a 3x2 grid of galaxy blocks (N = 5):
 
